41905317c76effc424f0531dae2de0ddd5290004
AlpacaDGY <76744548+AlpacaDGY@users.noreply.github.com>
2023-10-13 11:45:06 -0300

    Update Vapor.java

--- src/Vapor.java
+++ src/Vapor.java
@@ -4,13 +4,11 @@ import javax.swing.*;
 public class Vapor {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-        	TelaLogin login = new TelaLogin();
+        	VaporFrame vaporFrame = new VaporFrame();
+        	TelaLogin login = new TelaLogin(vaporFrame);
         	login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	login.setVisible(true);
-        	
-            //VaporFrame frame = new VaporFrame();
-            //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            //frame.setVisible(true);
         });
     }
 }
+
