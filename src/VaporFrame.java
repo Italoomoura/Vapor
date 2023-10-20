727143eaaf9502ce99c3c53ba2f4b40912bdb289
AlpacaDGY <76744548+AlpacaDGY@users.noreply.github.com>
2023-10-20 11:17:27 -0300

    Update VaporFrame.java

--- src/VaporFrame.java
+++ src/VaporFrame.java
@@ -4,22 +4,27 @@ import java.awt.*;
 public class VaporFrame extends JFrame {
     private JTabbedPane tabbedPane;
 
-    public VaporFrame() {
+    public VaporFrame(TelaLogin login) {
+    	
         setTitle("Vapor");
         setPreferredSize(new Dimension(800, 535));
         setResizable(false);
 
         tabbedPane = new JTabbedPane();
-
+        
         TelaBiblioteca telaBiblioteca = new TelaBiblioteca();
-        TelaLoja telaLoja = new TelaLoja(this, tabbedPane, telaBiblioteca); // Passa a instância de TelaBiblioteca para TelaLoja
+        TelaLoja telaLoja = new TelaLoja(this, tabbedPane, telaBiblioteca);
+        @SuppressWarnings("static-access")
+		TelaPerfil telaPerfil = new TelaPerfil(login.getUserName());
 
         tabbedPane.addTab("Loja", telaLoja);
-        tabbedPane.addTab("Biblioteca", telaBiblioteca); // Usa a mesma instância para a tela da biblioteca
+        tabbedPane.addTab("Biblioteca", telaBiblioteca);
+        tabbedPane.addTab("Meu perfil", telaPerfil);
 
         add(tabbedPane);
 
         pack();
         setLocationRelativeTo(null);
     }
+    
 }
